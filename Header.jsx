6160b95239ea84995f2b1f7804c1cd73e5fd8ed6
Armando Gjona <armandogjona.official@gmail.com>
2025-04-27 19:16:17 +0200

    initialize project and also created Header component with styling

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import chefMandoLogo from "./images/chefMando.png"
+
+export default function Header() {
+    return(
+        <header>
+            <img src = {chefMandoLogo} />
+            <h1>Chef Mando</h1>
+        </header>
+    )
+}
